715b1c7be810bd6d7a0bd2f6b3457450fc674f33
ZacharyDagnall <zachary.dagnall@gmail.com>
2021-01-15 11:59:37 -0500

    First attempt at testing

--- app/models/origin.rb
+++ app/models/origin.rb
@@ -1,3 +1,8 @@
 class Origin
-
+    attr_accessor :continent, :country
+    
+    def initialize(continent, country)
+        @continent = continent
+        @country = country
+    end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,10 +1,61 @@
 require_relative '../config/environment.rb'
+require_relative ""
 
 def reload
   load 'config/environment.rb'
 end
 
+#Saturday 12/16 12-2pm
+#Sunday 12/17  1pm
+#Monday 12/18  9am
+
+
+
 # think how many instances of what you need -- how to test your code in a best manner
+
+require "spec_helper"
+
+describe "Origin" do
+
+  # describe ".all" do
+  #   it "knows about all appointments that have been created" do
+  #     doctor_who = Doctor.new("The Doctor")
+  #     hevydevy = Patient.new("Devin Townsend")
+  #     doctor_who.new_appointment("Friday, January 32nd", hevydevy)
+  #     appointment = doctor_who.new_appointment("Caturday, January Purrty2nd", hevydevy)
+  #     doctor_who.new_appointment("Satunday, January 34nd", hevydevy)
+
+  #     expect(Appointment.all).to include(appointment)
+  #   end
+  # end
+
+  describe "#new" do
+    it "initializes with a continent and country" do
+      expect{origin = Origin.new("Europe","France")}.to_not raise_error
+    end
+  end
+
+  # describe "#patient" do
+  #   it "belongs to a patient" do
+  #     doctor_who = Doctor.new("The Doctor")
+  #     hevydevy = Patient.new("Devin Townsend")
+  #     appointment = hevydevy.new_appointment("Friday, January 32nd", doctor_who)
+  #     expect(appointment.patient).to eq(hevydevy)
+  #   end
+  # end
+
+  # describe "#doctor" do
+  #   it "belongs to a doctor" do
+  #     doctor_who = Doctor.new("The Doctor")
+  #     hevydevy = Patient.new("Devin Townsend")
+  #     appointment = doctor_who.new_appointment("Friday, January 32nd", hevydevy)
+  #     expect(appointment.doctor).to eq(doctor_who)
+  #   end
+  # end
+end
+
+
+
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
