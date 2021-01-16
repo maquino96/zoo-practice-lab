b243bb135532b51ca114ebb7341536e31cf21f01
maquino96 <matthewaquino96@gmail.com>
2021-01-16 12:37:51 -0500

    zoo

--- app/models/zoo.rb
+++ app/models/zoo.rb
@@ -1,3 +1,30 @@
 class Zoo
     
+    @@all = []
+
+    attr_accessor :name
+    attr_reader :address
+
+    def intiialized (name, address)
+        @name = name
+        @address = address
+    end 
+
+    def self.all
+        @@all
+    end 
+
+    def all_animal_species
+        Animals.all.map{&:species}.uniq
+    end 
+
+    def zoo_origins
+        Animals.all.select{|el| el.origin}.uniq
+    end 
+
+    def self.find_by_name(zoo_name)
+        self.class.all.select{|el| el.name == zoo_name }
+    end 
+
+
 end
