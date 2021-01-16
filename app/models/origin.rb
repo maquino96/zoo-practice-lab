1f9b86d22a89bbf77a71e382d845c1f1ef4c1e46
ZacharyDagnall <zachary.dagnall@gmail.com>
2021-01-16 13:44:04 -0500

    Deleted unneccessary comment

--- app/models/origin.rb
+++ app/models/origin.rb
@@ -36,18 +36,6 @@ class Origin
     end    
 
     def self.most_animals
-        #self.all.max{|a,b| a.animal_number <=> b.animal_number}
-        
-        # origin_most_animals = ""
-        # origin_most_animals_count = 0
-        # Origin.all.each do |origin|
-        #     if origin.animals.length > origin_most_animals_count
-        #         origin_most_animals = origin
-        #         origin_most_animals_count = origin.animals.length
-        #     end
-        # end
-        # origin_most_animals
-
         origin_numbers = self.all.map{|origin| origin.animal_number}
         self.all.find{|origin| origin.animal_number == origin_numbers.max}
     end    
