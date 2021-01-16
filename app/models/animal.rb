0c38e64ca73504e779829f07cec2fa1ca9063cfd
Jahaira Marrero <jahaira.marrero@gmail.com>
2021-01-16 12:07:19 -0500

    animal update

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,3 +1,24 @@
 class Animal
 
+    #The species and origin of the animal should not change.
+    #The name and zoo of the animal should be able to change.
+    #Instructions say we need to be able to create an array with all of the instances
+    attr_reader :species, :origin
+    attr_accessor :zoo, :name
+    @@all = []
+
+    #Needs to be instantiated with species and name
+    #When calling the species you should also get their zoo and origin
+    def initialize(species, name)
+        @species = species
+        @zoo = zoo
+        @origin = origin
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
 end
